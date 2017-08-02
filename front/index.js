75d4525c742fb8fde4daee8875ec9ec745478ad3
typicode <typicode@gmail.com>
2017-08-03 00:08:39 +0200

    wip

--- front/index.js
+++ front/index.js
@@ -1,21 +1,39 @@
-import './style'
 import { Component } from 'preact'
+import Server from './Server'
 
 export default class App extends Component {
   componentDidMount() {
     window.fetch('/_/servers').then(r => r.json()).then(json => {
       this.setState({
-        results: (json && json.items) || []
+        servers: json
       })
     })
   }
 
-  render(props, { results = [] }) {
+  render(props, { servers = {} }) {
     return (
-      <div>
-        <h1>Example</h1>
-        <div class="list">
-          {results.map(result => <Result result={result} />)}
+      <div class="flex flex-column vh-100 bg-black-90 white">
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/tachyons@4.7.0/css/tachyons.min.css"
+        />
+        <div class="bb pa3">hotel</div>
+        <div class="flex-auto flex">
+          <div class="w-third br">
+            {Object.keys(servers).map(id =>
+              <Server id={id} server={servers[id]} />
+            )}
+            {Object.keys(servers).map(id =>
+              <Server id={id} server={servers[id]} />
+            )}
+          </div>
+          <div class="w-two-thirds ">
+            <div class="flex bb">
+              <div class="pa3 br">Logs</div>
+              <div class="pa3 br">Conf</div>
+            </div>
+            <div class="pa3">No Output</div>
+          </div>
         </div>
       </div>
     )
