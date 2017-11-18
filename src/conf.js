f3a5e28c5ee68380de840c09d5cede292358ea89
Pavel Tsurbeleu <pavel.tsurbeleu@me.com>
2017-11-17 23:22:57 -0800

    Experimental support for 'Auto Proxy Discovery' (aka. WPAD) support in OSX

--- src/conf.js
+++ src/conf.js
@@ -8,7 +8,7 @@ mkdirp.sync(hotelDir)
 // Defaults
 const defaults = {
   port: 2000,
-  host: '127.0.0.1',
+  host: '0.0.0.0',
   timeout: 5000,
   tld: 'dev',
   // Replace with your network proxy IP (1.2.3.4:5000) if any
